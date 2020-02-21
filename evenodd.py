ff10084ec409a83a4c3e2ccc26548ea360e6e47e
Aishwarya2407 <52240784+Aishwarya2407@users.noreply.github.com>
2020-02-21 10:58:33 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,13 @@
+a=list(map(int,input()))
+for i in a:
+    if i%2==0:
+       c=str(i)
+       print(c,end="")
+for i in a:
+    if i%2!=0:
+        d=str(i)
+        print(d,end="")
+    
+    
+        
+    
