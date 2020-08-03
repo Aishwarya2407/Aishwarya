0353b965d6ff66fe09379f3d447c13a37e600902
Aishwarya2407 <52240784+Aishwarya2407@users.noreply.github.com>
2020-08-03 14:36:49 +0530

    Create pattern 9.py
    pattern by using square

---
+++ pattern 9.py
@@ -0,0 +1,10 @@
+# python-basics
+a=int(input())
+for i in range(1,a):
+    for i in range(0,i,1):
+        print((2**i),end=" ")
+    for i in range(-1+i,-1,-1):
+        print((2**i),end=" ")
+    print(" ")
+    
+    
